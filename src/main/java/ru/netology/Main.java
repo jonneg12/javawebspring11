44dddd4f9ea2bd5f736474733c849d1d7f7734dd
jonneg12 <jonneg12>
2021-02-06 10:30:01 +0700

    Refactoring server class structure

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,34 +1,19 @@
 package ru.netology;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         int port = 9999;
         int numberOfThreads = 64;
 
-        ExecutorService executorService = Executors.newFixedThreadPool(numberOfThreads);
-
         Server server = new Server(port, numberOfThreads);
 
-        ServerSocket serverSocket = server.getServerSocket();
+        server.listen();
 
-        while (true){
-            try {
-                Socket socket = serverSocket.accept();
-                executorService.submit(() -> server.connectionExecutor(socket));
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-        }
     }
 }
+
 
 
 
--- src/main/java/ru/netology/Server.java
+++ src/main/java/ru/netology/Server.java
@@ -11,75 +11,83 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
 
     private int port;
-    private int numberOfThreads;
+    private ExecutorService executorService;
 
     private final List<String> validPaths = Arrays.asList("/index.html", "/spring.svg", "/spring.png");
     private final String filesPath = "files";
 
     public Server(int port, int numberOfThreads) {
         this.port = port;
-        this.numberOfThreads = numberOfThreads;
+        this.executorService = Executors.newFixedThreadPool(numberOfThreads);
     }
 
-    public ServerSocket getServerSocket() throws IOException {
-        return new ServerSocket(port);
-    }
+    public void listen() {
 
-    public void connectionExecutor(Socket socket) {
-        try (
-                final BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                final BufferedOutputStream writer = new BufferedOutputStream(socket.getOutputStream())) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+                executorService.submit(() -> {
+                    try (
+                            final BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            final BufferedOutputStream writer = new BufferedOutputStream(socket.getOutputStream())) {
 
-            final String requestLine = reader.readLine();
-            final String[] parts = requestLine.split(" ");
+                        final String requestLine = reader.readLine();
+                        final String[] parts = requestLine.split(" ");
 
-            System.out.println("continue processing " + Thread.currentThread().getName());
+                        System.out.println("continue processing " + Thread.currentThread().getName());
 
-            //проверка правильности запроса
-            if (parts.length != 3) {
-                return;
+                        //проверка правильности запроса
+                        if (parts.length != 3) {
+                            return;
+                        }
+
+                        final String path = parts[1];
+                        System.out.println(path);
+                        if (!validPaths.contains(path)) {
+                            writer.write((
+                                    "HTTP/1.1 404 Not Found\r\n" +
+                                            "Content-Length: 0\r\n" +
+                                            "Connection: close\r\n" +
+                                            "\r\n"
+                            ).getBytes());
+                            writer.flush();
+                            System.out.println(404 + " not found");
+                            return;
+                        }
+
+                        final Path filePath = Paths.get(filesPath + path);
+                        final String mimeType = Files.probeContentType(filePath);
+                        final long length = Files.size(filePath);
+
+                        writer.write((
+                                "HTTP/1.1 200 OK\r\n" +
+                                        "Content type : " + mimeType + "\r\n" +
+                                        "Content-Length: " + length + "\r\n" +
+                                        "Connection: close\r\n" +
+                                        "\r\n"
+                        ).getBytes());
+                        Files.copy(filePath, writer);
+                        writer.flush();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        try {
+                            socket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
             }
-
-            final String path = parts[1];
-            System.out.println(path);
-            if (!validPaths.contains(path)) {
-                writer.write((
-                        "HTTP/1.1 404 Not Found\r\n" +
-                                "Content-Length: 0\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                writer.flush();
-                System.out.println(404 + " not found");
-                return;
-            }
-
-            final Path filePath = Paths.get(filesPath + path);
-            final String mimeType = Files.probeContentType(filePath);
-            final long length = Files.size(filePath);
-
-            writer.write((
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content type : " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            Files.copy(filePath, writer);
-            writer.flush();
-
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
